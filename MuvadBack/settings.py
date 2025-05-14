8514532a3c9442e183ef6d51244f41334d59c3f2
marvidik <ebubeidika@gmail.com>
2025-05-14 08:20:08 -0700

    sd

--- MuvadBack/settings.py
+++ MuvadBack/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 import os
 from pathlib import Path
 
+import pymysql
+pymysql.install_as_MySQLdb()
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -99,7 +103,7 @@ DATABASES = {
         'USER': 'muvapoxi_backend',
         'PASSWORD': 'Muvad@2024Ragolis',
         'HOST': 'localhost',  # or IP
-        # 'PORT': '3306',
+        'PORT': '3306',
     }
 }
 
