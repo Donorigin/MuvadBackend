47c4ea949601fdcfdd64a5bd0a4f3d81d261ee86
marvidik <ebubeidika@gmail.com>
2025-05-18 13:07:27 +0100

    changes

--- blog/models.py
+++ blog/models.py
@@ -5,21 +5,21 @@ from django.db import models
 
 
 class Blog(models.Model):
-    CATEGORY_CHOICES = [
-        ('Cleaning', 'Cleaning'),
-        ("Drywall","Drywall"),
-        ("Painting","Painting"),
-        ("Concrete","Concrete"),
-        ("Roofing","Roofing"),
-        ("Stucco","Stucco")
-        # Add more categories as needed
-    ]
+    # CATEGORY_CHOICES = [
+    #     ('Cleaning', 'Cleaning'),
+    #     ("Drywall","Drywall"),
+    #     ("Painting","Painting"),
+    #     ("Concrete","Concrete"),
+    #     ("Roofing","Roofing"),
+    #     ("Stucco","Stucco")
+    #     # Add more categories as needed
+    # ]
 
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=100)
     content = models.TextField()
     image = models.ImageField(upload_to='blog-images/', blank=True, null=True)
-    category = models.CharField(max_length=50, choices=CATEGORY_CHOICES)
+    category = models.CharField(max_length=50)
     time_to_read = models.CharField(max_length=20)  # e.g., "4 min read"
     published_date = models.DateField()
     created_at = models.DateTimeField(auto_now_add=True)
