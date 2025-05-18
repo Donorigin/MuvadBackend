7e4683fb6922c4da612c74f40ee3567e3fe67c08
marvidik <ebubeidika@gmail.com>
2025-05-18 12:30:13 +0100

    change the db

--- MuvadBack/settings.py
+++ MuvadBack/settings.py
@@ -89,23 +89,23 @@ WSGI_APPLICATION = 'MuvadBack.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'muvapoxi_MuvadBackend',
-#         'USER': 'muvapoxi_backend',
-#         'PASSWORD': 'Muvad@2024Ragolis',
-#         'HOST': 'localhost',  # or IP
-#         'PORT': '3306',
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'muvapoxi_MuvadBackend',
+        'USER': 'muvapoxi_backend',
+        'PASSWORD': 'Muvad@2024Ragolis',
+        'HOST': 'localhost',  # or IP
+        'PORT': '3306',
+    }
+}
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
